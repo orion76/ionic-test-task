c20b34e9b971916a5f6e145a1d60be1d8b1acfcd
Paul <gpb@yandex.ru>
2021-10-21 11:30:34 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-task-store',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
